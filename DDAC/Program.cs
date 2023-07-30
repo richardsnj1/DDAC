a0f80834b13b28eb29fd14a2fc4c6a6e092a901d
Elden Zheng <elden.zheng@gmail.com>
2023-07-30 21:35:32 +0800

    added register and log in

--- DDAC/Program.cs
+++ DDAC/Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using DDAC.Data;
+using DDAC.Areas.Identity.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DDACContextConnection") ?? throw new InvalidOperationException("Connection string 'DDACContextConnection' not found.");
+
+builder.Services.AddDbContext<DDACContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<DDACUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<DDACContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -17,11 +28,13 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication(); //direct to login page
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
